4e51c9c65760bd39577e697711b11b76ad6f3cba
inzahan <inzahanyang@gmail.com>
2022-03-01 11:12:51 +0900

    community

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Upload: NextPage = () => {
   return (
-    <div className="px-4 py-16">
+    <div className="px-4 py-10">
       <div>
         <label className="flex h-48 w-full cursor-pointer items-center justify-center rounded-md border-2 border-dashed border-gray-300 text-gray-600 hover:border-orange-500 hover:text-orange-500">
           <svg
@@ -22,40 +22,39 @@ const Upload: NextPage = () => {
           <input className="hidden" type="file" />
         </label>
       </div>
+
       <div className="my-5">
-        <label
-          className="mb-1 block text-sm font-medium text-gray-700"
-          htmlFor="price"
-        >
-          Price
-        </label>
-        <div className="relative flex items-center  rounded-md shadow-sm">
+        <label className="mb-1 block font-medium">Price</label>
+
+        <div className="relative flex items-center rounded-md shadow-sm">
           <div className="pointer-events-none absolute left-0 flex items-center justify-center pl-3">
             <span className="text-sm text-gray-500">$</span>
           </div>
+
           <input
-            id="price"
-            className="w-full appearance-none rounded-md border border-gray-300 px-3 py-2 pl-7 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+            className="w-full appearance-none rounded-md border border-gray-300 px-3 py-2 pl-7 placeholder-gray-400 shadow-sm  focus:border-orange-500 focus:outline-none focus:ring-orange-500"
             type="text"
             placeholder="0.00"
           />
+
           <div className="pointer-events-none absolute right-0 flex items-center pr-3">
             <span className="text-gray-500">USD</span>
           </div>
         </div>
       </div>
+
       <div>
         <label className="mb-1 block text-sm font-medium text-gray-700">
           Description
         </label>
-
         <textarea
-          className="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500 "
+          className="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500"
           rows={4}
         />
       </div>
-      <button className="mt-5 w-full rounded-md border border-transparent bg-orange-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 ">
-        Upload product
+
+      <button className="mt-3 w-full rounded-md border border-transparent bg-orange-500 py-2 px-4 font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 active:scale-95">
+        Upload Product
       </button>
     </div>
   );
