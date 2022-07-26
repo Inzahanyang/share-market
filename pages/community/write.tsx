8b66b3b09405a983f2f950eb059f208f8122b17b
inzahan <inzahanyang@gmail.com>
2022-07-27 06:54:43 +0900

    geoLocation

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -2,13 +2,51 @@ import type { NextPage } from "next";
 import Button from "@components/button";
 import Layout from "@components/layout";
 import Textarea from "@components/textarea";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutation";
+import { Post } from "@prisma/client";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import useCoords from "@libs/client/useCoords";
+
+interface WriteFrom {
+  question: string;
+}
+
+interface WriteResponse {
+  ok: boolean;
+  post: Post;
+}
 
 const Write: NextPage = () => {
+  const { latitude, longitude } = useCoords();
+
+  const router = useRouter();
+
+  const { register, handleSubmit } = useForm<WriteFrom>();
+
+  const [post, { loading, data }] = useMutation<WriteResponse>("/api/posts");
+
+  const onValid = (data: WriteFrom) => {
+    if (loading) return;
+    post({ ...data, latitude, longitude });
+  };
+
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/community`);
+    }
+  }, [data, router]);
+
   return (
     <Layout canGoBack title="Write Post">
-      <form className="space-y-4 p-4">
-        <Textarea required placeholder="Ask a question!" />
-        <Button text="Submit" />
+      <form onSubmit={handleSubmit(onValid)} className="space-y-4 p-4">
+        <Textarea
+          register={register("question", { required: true, minLength: 5 })}
+          required
+          placeholder="Ask a question!"
+        />
+        <Button text={loading ? "Loading..." : "Submit"} />
       </form>
     </Layout>
   );
