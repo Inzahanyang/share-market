bc9790dbe1a4bd98fc88460a7497c16cf7df14c0
inzahan <inzahanyang@gmail.com>
2022-07-28 08:23:25 +0900

    model add

---
+++ pages/api/reviews.ts
@@ -0,0 +1,36 @@
+import withHandler from "@libs/server/withHandler";
+import { withApiSession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    session: { user },
+  } = req;
+
+  const reviews = await client.review.findMany({
+    where: {
+      createdForId: user?.id,
+    },
+    include: {
+      createdBy: { select: { id: true, name: true, avatar: true } },
+    },
+  });
+
+  if (!reviews)
+    res.status(404).json({ ok: false, error: "There is no reviews..." });
+
+  console.log(reviews);
+
+  res.json({
+    ok: true,
+    reviews,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
