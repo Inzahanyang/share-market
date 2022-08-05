d1076d94b89f1ad49ffde955fca9699117ce41ea
inzahan <inzahanyang@gmail.com>
2022-08-05 11:15:13 +0900

    upload product with img

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -29,7 +29,7 @@ async function handler(
   }
   if (req.method === "POST") {
     const {
-      body: { name, price, description },
+      body: { name, price, description, photoId },
       session: { user },
     } = req;
 
@@ -38,7 +38,7 @@ async function handler(
         name,
         price: +price,
         description,
-        image: "xx",
+        image: photoId,
         user: {
           connect: {
             id: user?.id,
