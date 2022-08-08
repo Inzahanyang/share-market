a84d1d9d3041542589e2a772c15005373c259268
inzahan <inzahanyang@gmail.com>
2022-08-08 23:13:06 +0900

    Nextjs Script strategy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 import "react-loading-skeleton/dist/skeleton.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   console.log("App is Running");
@@ -12,6 +13,23 @@ function MyApp({ Component, pageProps }: AppProps) {
       <div className="mx-auto w-full max-w-xl">
         <Component {...pageProps} />
       </div>
+      <Script
+        src="https://developers.kakao.com/sdk/js/kakao.js"
+        strategy="lazyOnload"
+      />
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        onLoad={() => {
+          window.fbAsyncInit = function () {
+            FB.init({
+              appId: "your-app-id",
+              autoLogAppEvents: true,
+              xfbml: true,
+              version: "v14.0",
+            });
+          };
+        }}
+      />
     </SWRConfig>
   );
 }
