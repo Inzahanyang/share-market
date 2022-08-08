0356113b5c93da2186d1d3e856a7217abd694a8e
inzahan <inzahanyang@gmail.com>
2022-08-08 13:12:17 +0900

    middleware with auth

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,13 @@
-import type { NextRequest, NextFetchEvent } from "next/server";
+import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log(req.ua);
+  if (req.ua?.isBot) {
+    return new Response("Plz don't be a bot");
+  }
+
+  if (!req.url.includes("/api")) {
+    if (!req.url.includes(`/enter`) && !req.cookies.sharemarket) {
+      return NextResponse.redirect(`${req.nextUrl.origin}/enter`);
+    }
+  }
 }
